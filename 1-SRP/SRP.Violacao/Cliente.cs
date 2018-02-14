4fe3570ed6bc1c21f63d43c43a9dd69e094f440a
Ronye <ronye.rocha@gmail.com>
2018-02-14 21:49:37 -0200

    AdicionaCliente

--- 1-SRP/SRP.Violacao/Cliente.cs
+++ 1-SRP/SRP.Violacao/Cliente.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +13,39 @@ namespace SOLID._1_SRP.SRP.Violacao
         public int ClienteId { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
-        public int Cpf { get; set; }
+        public string Cpf { get; set; }
         public DateTime DataCadastro { get; set; }
 
+        public string AdicionarCliente()
+        {
+            if (!Email.Contains("@"))
+            {
+                return "Email inválido";
+            }
+
+            if (Cpf.Length != 11)
+            {
+                return "CPF inválido!";
+            }
+
+            using (var connection = new SqlConnection())
+            {
+                using (var command = new SqlCommand())
+                {
+                    connection.ConnectionString = @"MinhaConnectionString";
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = @"INSERT INTO CLIENTE (NOME, EMAIL, CPF, DATACADASTRO VALUES(@nome, @email, @cpf, @dataCadastro)";
+
+                    command.Parameters.AddWithValue("nome", Nome);
+                    command.Parameters.AddWithValue("email", Email);
+                    command.Parameters.AddWithValue("cpf", Cpf);
+                    command.Parameters.AddWithValue("dataCadastro", DataCadastro);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
