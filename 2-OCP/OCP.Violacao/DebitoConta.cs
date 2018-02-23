290d5ef61869d3b0bbee1d0ea68272e1edef021d
Ronye <ronye.rocha@gmail.com>
2018-02-22 21:00:19 -0300

    DebitoConta

--- 2-OCP/OCP.Violacao/DebitoConta.cs
+++ 2-OCP/OCP.Violacao/DebitoConta.cs
@@ -8,5 +8,18 @@ namespace SOLID._2_OCP.OCP.Violacao
 {
     class DebitoConta
     {
+        public void Deletar(decimal valor, string conta, TipoConta tipoConta)
+        {
+            if (tipoConta == TipoConta.Corrente)
+            {
+                // Debita Conta Corrente
+            }
+
+            if (tipoConta == TipoConta.Poupanca)
+            {
+                // Valida aniversário da conta
+                // Debita conta poupança
+            }
+        }
     }
 }
