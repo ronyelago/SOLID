8cb342817a660b78d5bd68a251bb0f7bf70b3929
Ronye <ronye.rocha@gmail.com>
2018-03-07 20:51:12 -0300

    Retangulo

--- 3-LSP/Retangulo.cs
+++ 3-LSP/Retangulo.cs
@@ -8,5 +8,14 @@ namespace SOLID._3_LSP
 {
     class Retangulo
     {
+        public virtual double Altura { get; set; }
+        public virtual double Largura { get; set; }
+        public double Area
+        {
+            get
+            {
+                return Altura * Largura;
+            }
+        }
     }
 }
